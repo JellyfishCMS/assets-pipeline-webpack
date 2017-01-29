e50d302ee34839450b9cbc8879cda6db7096603b
xNok <nokwebspace@gmail.com>
2017-01-29 18:30:29 -0500

    image loader

--- webpack.conf/webpack.base.js
+++ webpack.conf/webpack.base.js
@@ -11,6 +11,7 @@ const NpmInstallPlugin = require('npm-install-webpack2-plugin')
 // loaders configurations
 const babelLoader = require('../webpack.mod/babel-loader')
 const cssLoader   = require('../webpack.mod/css-loader')
+const imageLoader = require('../webpack.mod/image-loader')
 
 /**
 ** WEBPACK configuration object
@@ -45,7 +46,8 @@ let webpack_base = {
     rules: [
       // rules for modules (configure loaders, parser options, etc.)
       babelLoader,
-      cssLoader
+      cssLoader,
+      imageLoader
     ]
   },
     //> modules and loaders
