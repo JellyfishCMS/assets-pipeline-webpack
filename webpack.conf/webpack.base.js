a1cb2d0f104b75f1372d39a1efcd1e1046acd3f7
xNok <nokwebspace@gmail.com>
2017-07-10 20:39:36 -0400

    improve stage 0 comments

--- webpack.conf/webpack.base.js
+++ webpack.conf/webpack.base.js
@@ -13,84 +13,86 @@ const NpmInstallPlugin = require('npm-install-webpack2-plugin')
 ****************************/
 let webpack_base = {
   entry: config.entry, // string | array | object
-    // Here the application starts executing and webpack starts bundling
+    //> Here the application starts executing and webpack starts bundling
 
   output: {
     path: path.resolve(config.output.path), // string
-    // the target directory for all output files
-    // must be an absolute path (use the Node.js path module)
+      //> the target directory for all output files
+      //> must be an absolute path (use the Node.js path module)
 
     filename: config.debug ? '[name].js' : '[name].[chunkhash:8].js', // string
-    // the filename template for our bundles
+      //> the filename template for our bundles
 
     publicPath: config.output.publicPath, // string
-    // the url to the output directory resolved relative to the HTML page
+      //> the url to the output directory resolved relative to the HTML page
 
     //library: "MyLibrary", // string
-    // the name of the exported library
+      //> the name of the exported library
 
     //libraryTarget: "umd", // enum
-    // the type of the exported library
+      //> the type of the exported library
 
     /* Advanced output configuration (see: https://webpack.js.org/configuration/) */
 
   },
-    // options related how webpack emits results
+    //> options related how webpack emits results
 
   module: {
     rules: [
-      { test: /\.css$/, loader: "style-loader" }
+      // ...
     ]
   },
-    // modules and loaders
+    //> modules and loaders
 
   resolve: {
 
     //modules: [],
-    // directories where to look for modules
+      //> directories where to look for modules
 
     extensions: ['.js', '.css', '.json'],
-    // extensions that are used
+      //> extensions that are used
 
     alias: {}
-    // a list of module name aliases
+      //> a list of module name aliases
 
     /* Advanced resolve configuration (see https://webpack.js.org/configuration/) */
 
   },
-    // options for resolving module requests
-    // (does not apply to resolving to loaders)
+    //> options for resolving module requests
+    //> (does not apply to resolving to loaders)
 
   performance: {
     hints: config.debug ? false : 'warning'
   },
-    // Configure how performance hints are shown.
+    //> Configure how performance hints are shown.
 
   devtool: config.debug ? 'cheap-module-eval-source-map' : false,
-    // enhance debugging by adding meta info for the browser devtools
-    // source-map most detailed at the expense of build speed.
+    //> enhance debugging by adding meta info for the browser devtools
+    //> source-map most detailed at the expense of build speed.
 
   //context: path.resolve(__dirname, config.context),
-    // the home directory for webpack
-    // the entry and module.rules.loader option
-    // is resolved relative to this directory
+    //> the home directory for webpack
+    //> the entry and module.rules.loader option
+    //> is resolved relative to this directory
 
   target: config.target,
-    // the environment in which the bundle should run
-    // changes chunk loading behavior and available modules
+    //> the environment in which the bundle should run
+    //> changes chunk loading behavior and available modules
 
   stats: "normal",
-    //lets you precisely control what bundle information gets displayed.
+    //> lets you precisely control what bundle information gets displayed.
 
   devServer: {},
-    //This set of options is picked up by webpack-dev-server and can be used to change it's behavior in various ways.
+    //> This set of options is picked up by webpack-dev-server
+    //> and can be used to change it's behavior in various ways.
 
   watch: true,
+    //> Watch files for modification and run webpack when updated
 
   plugins: [
     new NpmInstallPlugin()
   ],
-    // list of additional plugins
+    //> list of additional plugins
 }
 
 module.exports = webpack_base
